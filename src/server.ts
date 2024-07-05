09571fe804335380b37dc42ee390f8722f15b77b
José Bonifácio <boni@MacBook.local>
2024-07-05 12:06:08 -0300

    remove tmp

--- src/server.ts
+++ src/server.ts
@@ -31,4 +31,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(3333, () => console.log("rodando v6"))
+app.listen(3333, () => console.log("rodando v6.1"))
